4ee2e10f6ef0051a4c4a4a8144dd4f951487458c
Tamotsu Takahashi <ttakah+github@gmail.com>
2024-03-13 23:49:38 +0900

    No need for TrustedPublisher

--- uia/installcert.ps1
+++ uia/installcert.ps1
@@ -1,13 +1,6 @@
 Param([String]$cert = "$PSScriptRoot\code_signing.crt")
-Import-Certificate -FilePath $cert -Cert Cert:\CurrentUser\TrustedPublisher
 Import-Certificate -FilePath $cert -Cert Cert:\CurrentUser\Root
 
 # Use mmc.exe to remove the certs because Remove-Item cannot uninstall root certs
-# because
-#   Get-ChildItem Cert:\CurrentUser\Root             -DnsName BlackSpotPerMonitor.ahk | Remove-Item
+#   Get-ChildItem Cert:\CurrentUser\Root -DnsName BlackSpotPerMonitor.ahk | Remove-Item
 # will fail
-# though
-#   Get-ChildItem Cert:\CurrentUser\TrustedPublisher -DnsName BlackSpotPerMonitor.ahk | Remove-Item
-# and
-#   Get-ChildItem Cert:\CurrentUser\CA               -DnsName BlackSpotPerMonitor.ahk | Remove-Item
-# will succeed
